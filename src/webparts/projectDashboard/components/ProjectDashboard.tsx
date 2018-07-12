23247544863cc7a8f6980529231ac7ca8cca94e0
sachinsarse <sachin.sarse@eternussolutions.com>
2018-07-12 16:48:40 +0530

    Integrated CeoDashboard and primetable

--- src/webparts/projectDashboard/components/ProjectDashboard.tsx
+++ src/webparts/projectDashboard/components/ProjectDashboard.tsx
@@ -1,24 +1,29 @@
 import * as React from 'react';
 import styles from './ProjectDashboard.module.scss';
-import { IProjectDashboardProps } from './IProjectDashboardProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
+import { IProjectDashboardProps } from './IProjectDashboardProps';
+import CEODashboard from './CEODashboard/CEODashbaord';
 export default class ProjectDashboard extends React.Component<IProjectDashboardProps, {}> {
   public render(): React.ReactElement<IProjectDashboardProps> {
     return (
-      <div className={ styles.projectDashboard }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
+      // <div className={ styles.projectDashboard }>
+      //   <div className={ styles.container }>
+      //     <div className={ styles.row }>
+      //       <div className={ styles.column }>
+      //         <span className={ styles.title }>Welcome to SharePoint!</span>
+      //         <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
+      //         <p className={ styles.description }>{escape(this.props.description)}</p>
+      //         <a href="https://aka.ms/spfx" className={ styles.button }>
+      //           <span className={ styles.label }>Learn more</span>
+      //         </a>
+      //       </div>
+      //     </div>
+      //   </div>
+      // </div>
+      <div>
+        <CEODashboard></CEODashboard>
       </div>
     );
   }

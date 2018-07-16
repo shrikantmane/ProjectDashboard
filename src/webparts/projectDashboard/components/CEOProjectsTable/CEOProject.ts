c542407c5ba36e896b062d88492b5884cd64ba0d
sachinsarse <sachin.sarse@eternussolutions.com>
2018-07-16 21:11:28 +0530

    Integrated Html UI and Css with CEO Dashboard

--- src/webparts/projectDashboard/components/CEOProjectsTable/CEOProject.ts
+++ src/webparts/projectDashboard/components/CEOProjectsTable/CEOProject.ts
@@ -19,6 +19,52 @@ export class CEOProjects {
     public Status0: Status;
     public StartDate :string;
     public DueDate: string;
-    public MildStone : string;
+    public MildStone : MildStones;
     public AssignedTo: Array<AssignedTo>;
+    public MildStoneList: Array<MildStones>;
+    public TagList: Array<Tags>;
+    public TeamMemberList: Array<TeamMembers>;
+    public DocumentList: Array<Documents>;
+}
+
+export class Project {
+    public Title: string;   
+    public ID :number;   
+}
+export class MildStones {
+    public Title: string;   
+    public StartDate :string;
+    public DueDate: string;
+    public Status0: Status;
+    public Project: Project;
+    public AssignedTo: Array<AssignedTo>;
+    public Priority: string;
+    public Body: string;
+}
+
+export class Tags {
+    public ID: number;
+    public Tag: string;
+    public Title:string;   
+}
+
+export class Team_x0020_Member {
+    Email : string;
+    ImgUrl: string
+}
+export class TeamMembers {
+    public ID: number;
+    public Project: Project;
+    public Start_x0020_Date: string;
+    public End_x0020_Date: Status;
+    public Status :string;
+    public Team_x0020_Member: Team_x0020_Member;
+}
+
+export class File {
+    public Name : string;
+    public LinkingUri : string;
+}
+export class Documents {
+    public File : File;
 }

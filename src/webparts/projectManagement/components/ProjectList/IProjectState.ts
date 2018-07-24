edf48238187121e1584dae67507e9ce52dec8d6c
Chetankumar Badgujar <chetan.badgujar@Eternus.local>
2018-07-24 12:10:38 +0530

    Added Project List

---
+++ src/webparts/projectManagement/components/ProjectList/IProjectState.ts
@@ -0,0 +1,18 @@
+ import { Project } from "./ProjectList";
+// import ProjectTimeLine  from "../CEOProjectTimeLine/ProjectTimeLine";
+
+export interface IProjectListState {
+    projectList: Array<Project>,
+    //   projectTimeLine: Array<ProjectTimeLine>,
+    //   expandedRows? :any,
+    //   globalFilter?: any,
+    //   projectName: string,
+    //   ownerName: string,
+    //   status: string,
+    //   priority: string,
+    //   isLoading: boolean,
+    //   isTeamMemberLoaded : boolean,
+    //   isKeyDocumentLoaded :boolean,
+    //   isTagLoaded :boolean,
+    //   expandedRowID : number,
+}

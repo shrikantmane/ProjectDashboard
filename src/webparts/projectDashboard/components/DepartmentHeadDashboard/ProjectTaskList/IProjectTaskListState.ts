45b3b0fea39e2e97b10e95af5ad9889a25d5746b
Chetankumar Badgujar <chetan.badgujar@Eternus.local>
2018-08-10 20:52:34 +0530

    Added document panel

--- src/webparts/projectDashboard/components/DepartmentHeadDashboard/ProjectTaskList/IProjectTaskListState.ts
+++ src/webparts/projectDashboard/components/DepartmentHeadDashboard/ProjectTaskList/IProjectTaskListState.ts
@@ -3,5 +3,7 @@ export interface IProjectTaskListState {
     description ?: string;
     taskList :  Array<Task>;
     showComponent: boolean,
+    showDocumentComponent: boolean,
     taskID ?: number,
+    documentID ?: number,
   }

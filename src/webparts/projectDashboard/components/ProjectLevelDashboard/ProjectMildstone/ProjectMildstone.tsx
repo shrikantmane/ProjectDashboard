2dc65acbef3c6cf385ad4acf02aca2d3aa017a30
espl0510 <rohit.nagpal@eternussolutions.com>
2018-08-14 09:36:37 +0530

    changes in progress bar and event update and css changes for dashboard

--- src/webparts/projectDashboard/components/ProjectLevelDashboard/ProjectMildstone/ProjectMildstone.tsx
+++ src/webparts/projectDashboard/components/ProjectLevelDashboard/ProjectMildstone/ProjectMildstone.tsx
@@ -6,13 +6,14 @@ import { IProjectMildstoneProps } from './IProjectMildstoneProps';
 import { IProjectMildstoneState } from './IProjectMildstoneState';
 import { Milestone } from '../Project';
 import styles from "../../ProjectDashboard.module.scss";
-
+let progress:number;
 export default class ProjectMildstone extends React.Component<IProjectMildstoneProps, IProjectMildstoneState> {
 
   constructor(props) {
     super(props);
     this.state = {
-      milstoneList: new Array<Milestone>()
+      milstoneList: new Array<Milestone>(),
+      progressbar:null
     };
   }
 
@@ -31,12 +32,27 @@ export default class ProjectMildstone extends React.Component<IProjectMildstoneP
   }
 
   private getAllMilestones(scheduleList: string) {
+    let temp1:any;
+    let temp2:any;
+    let temp3:number;
+    let temp4:number;
     sp.web.lists.getByTitle(scheduleList).items
       .select('Title', 'DueDate', 'Status0/ID', 'Status0/Status', 'Status0/Status_x0020_Color', 'Priority').expand('Status0')
       .filter("Duration eq '0 days'")
       .get()
       .then((mildstones: Array<Milestone>) => {
+        temp1=mildstones;
         this.setState({ milstoneList: mildstones })
+       
+    temp2=temp1.filter(item => item.Status0.Status == "Done" );
+    temp3=temp1.length;
+    temp4=temp2.length;
+      console.log("a",temp1);
+      console.log("b2",temp2)
+      progress=((temp4/temp3)*100);
+        console.log("milestones",mildstones)
+        console.log("progress1",progress);
+        this.setState({ progressbar: progress })
       });
   }
 
@@ -44,7 +60,7 @@ export default class ProjectMildstone extends React.Component<IProjectMildstoneP
     return (
       <div className="projectHealth">
         <div className="row">
-          <div className="col-sm-4 col-12">
+          <div className="col-sm-3 col-12">
             <div className="row dark-blue">
 
               <div className="status-title">
@@ -52,14 +68,14 @@ export default class ProjectMildstone extends React.Component<IProjectMildstoneP
               </div>
 
               <div id="skill" className="mid-text mid-text col-sm-12 col-12">
-                <h5>80% Of Project is Done</h5>
+                <h5>{this.state.progressbar}% Of Project is Done</h5>
                 {/* <div className="progress-bar-outline"><span className="bar jquery"></span><h5>In Progress</h5></div> */}
-                <ProgressBar bsClass="bar jquery" bsStyle="warning" now={80} label="" />
+                <ProgressBar bsClass="bar jquery" bsStyle="warning" now={this.state.progressbar} label="" />
                 <div><h5>In Progress</h5></div>
               </div>
             </div>
           </div>
-          <div className="col-sm-8 col-12 all-milestones container-outer">
+          <div className="col-sm-9 col-12 all-milestones container-outer">
             <div className="row container-inner">
               {this.state.milstoneList != null
                 ? this.state.milstoneList.map((item, key) => {
